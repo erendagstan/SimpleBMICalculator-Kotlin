1dd6d2ccdbe0819f55877c01b7687e8094bdfc11
Mert Eren Dağıstan <dagstaneren@gmail.com>
2023-03-24 03:34:03 +0300

    Project uploaded v1.0

---
+++ app/src/main/java/com/erendagistan/bmicalculator/MainActivity.kt
@@ -0,0 +1,92 @@
+package com.erendagistan.bmicalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.erendagistan.bmicalculator.databinding.ActivityMainBinding
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+//        val weightText=findViewById<EditText>(R.id.etWeight)
+//        val heightText=findViewById<EditText>(R.id.etHeight)
+//        val calculateBtn=findViewById<Button>(R.id.calculateBtn)
+
+        binding.calculateBtn.setOnClickListener {
+            val weight=binding.etWeight.text.toString()
+            val height = binding.etHeight.text.toString()
+            if(validateInput(weight,height)){
+                val bmi=weight.toFloat()/((height.toFloat()/100)*(height.toFloat()/100))
+                val bmi2Digits = String.format("%.2f",bmi).toFloat()   //get result with two decimal places
+                displayResult(bmi2Digits)
+            }
+        }
+    }
+
+    private fun validateInput(weight:String?,height:String?) :Boolean {
+        return when{
+            weight.isNullOrEmpty()->{
+                Toast.makeText(this,"Weight is empty",Toast.LENGTH_LONG).show()
+                return false
+            }
+            height.isNullOrEmpty()->{
+                Toast.makeText(this,"Height is empty",Toast.LENGTH_LONG).show()
+                return false
+            }
+            else -> {
+                return true
+            }
+        }
+    }
+
+    private fun displayResult(bmi:Float){
+//        val resultIndex=findViewById<TextView>(R.id.tvIndex)
+//        val resultDescription=findViewById<TextView>(R.id.tvResult)
+//        val resultInfo=findViewById<TextView>(R.id.tvInfo)
+
+        binding.tvIndex.text=bmi.toString()
+        binding.tvInfo.text="(Normal range is 18.5-24.9)"
+
+
+        //resultIndex.text=bmi.toString()
+        //resultInfo.text="(Normal range is 18.5-24.9)"
+
+        var resultText=""
+        var color = 0
+
+        when{
+            bmi<18.50 ->{
+                resultText="Underweight"
+                color=R.color.underweight
+            }
+            bmi in 18.50..24.99 ->{
+                resultText="Normal"
+                color=R.color.normal
+            }
+            bmi in 25.00..29.99 ->{
+                resultText="Overweight"
+                color=R.color.overweight
+            }
+            bmi>29.99 ->{
+                resultText="Obese"
+                color=R.color.obese
+            }
+        }
+        binding.tvResult.setTextColor(ContextCompat.getColor(this,color))
+        binding.tvResult.text=resultText
+
+
+    }
+
+
+
+}
